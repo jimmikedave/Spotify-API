cdcd9612a8cfa0e622a44876d1d00ebfcd2fb285
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2020-09-22 21:38:43 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,9 @@ class App extends Component {
       nowPlaying: {
         name: 'Not Checked',
         image: ''
+      },
+      top50: {
+        list: []
       }
     }
     if(params.access_token){
@@ -33,7 +36,6 @@ class App extends Component {
   getNowPlaying() {
     spotifyWebApi.getMyCurrentPlaybackState()
     .then((response) => {
-      console.log(response)
       this.setState({
         nowPlaying: {
           name: response.item.name,
@@ -43,17 +45,64 @@ class App extends Component {
     })
   }
 
+  getMyTopTracks() {
+    spotifyWebApi.getMyTopTracks({limit: 50})
+    .then((response) => {
+      
+      var i;
+
+      let top50List = [];
+
+      for(i = 0; i < response.items.length; i++) {
+        top50List.push(response.items[i].name)
+      }
+
+      if(top50List.length === 50) {
+        this.setState({
+          top50: {
+            list: top50List
+          }
+        })
+        
+        for(i = 0; i < this.state.top50.list.length; i++) {
+          this.createListItem(this.state.top50.list[i])
+        }
+      }
+    })
+  }
+
+  createListItem(song) {
+    const orderedList = document.getElementById("top50");
+    const listItem = document.createElement("li");
+
+    listItem.textContent = song;
+
+    orderedList.appendChild(listItem)
+  }
+
   render() {
     return (
       <div className="App">
         <a href='http://localhost:8888'><button>Login with Spotify</button></a>
-        <div>Now Playing: {this.state.nowPlaying.name}</div>
         <div>
-          <img src={this.state.nowPlaying.image} style={{width: 100}}/>
+          <h1>Now Playing: {this.state.nowPlaying.name}</h1>
         </div>
-        <button onClick={() => this.getNowPlaying()}>
+        <div>
+          <img src={this.state.nowPlaying.image} style={{width: 400}}/>
+        </div>
+        <button onClick={() => {
+          this.getNowPlaying()
+          }}>
           Check Now Playing
         </button>
+        <button onClick={() => {
+          this.getMyTopTracks()
+          }}>
+          Show Top 50 Songs
+        </button>
+          <div>
+            <ol id="top50"></ol>
+          </div>
       </div>
     );
   }
